f5acfa6d79e4df4afa1b302681af8d71b5fc1dfe
ishika <ishikatakhtani04@gmail.com>
2025-02-17 10:43:14 +0530

    update

---
+++ proj.js
@@ -0,0 +1,136 @@
+let sub1=()=>{
+    let ipnumber=document.querySelector("#num").value;
+    let ippassword=document.querySelector("#pass").value;
+
+    let errnumber=document.querySelector("#errornum");
+    let errpass=document.querySelector("#errorpass");
+
+     if(ipnumber=="" ){
+
+        
+      errnumber.innerHTML="please enter your number"
+      errnumber.style.color="darkblue"
+      return false
+  }
+  else if( isNaN(ipnumber)){
+     
+      errnumber.innerHTML="please enter number only"
+      errnumber.style.color="darkblue"
+      return false
+  }
+  else if(ipnumber.length!==10){
+      
+      errnumber.innerHTML="please enter 10 digit number "
+      errnumber.style.color="darkblue"
+      return false
+  }
+
+  else if(ippassword=="" ){
+    errnumber.innerHTML=""
+        errpass.innerHTML="please enter your password"
+        errpass.style.color="darkblue"
+        return false
+        
+}
+else if(!( ippassword.match([/1234567890/]&&ippassword.match([/ !@#$%^&*()/])&&ippassword.match([/ a-z/])&&ippassword.match([/ A-Z/])))){
+    errpass.innerHTML="please enter correct password"
+    errpass.style.color="green"
+    return false  
+}
+      
+}
+
+let sub=()=>{
+    let ipname=document.querySelector("#Fname").value;
+    let Lipname=document.querySelector("#Lname").value;
+let ipnumber=document.querySelector("#num").value;
+let ipemail=document.querySelector("#Email").value;
+let ippassword=document.querySelector("#pass").value;
+let ipcpass=document.querySelector("#cpass").value;
+
+let errname=document.querySelector("#errorname");
+let errlname=document.querySelector("#errorlname")
+let errnumber=document.querySelector("#errornum");
+let erremail=document.querySelector("#errmail");
+let errpass=document.querySelector("#errorpass");
+let errcpass=document.querySelector("#errorcpass");
+if(ipname==""){
+    //  let nameinput=document.querySelector("#Fname")
+    //  nameinput.style.border= "1px solid red"
+    // nameinput.placeholder="enter name "
+    errname.innerHTML="please enter you name"
+    errname.style.color="red"
+    return false
+}
+if(Lipname==""){
+    //  let nameinput=document.querySelector("#name")
+    //  nameinput.style.border= "1px solid red"
+    // nameinput.placeholder="enter name "
+    errlname.innerHTML="please enter you name"
+    errlname.style.color="red"
+    return false
+}
+
+
+
+else if(ipemail==""){
+    //  errnumber.innerHTML=""
+    erremail.innerHTML="please enter your email"
+    erremail.style.color="red"
+    return false
+}
+else if(!(ipemail.includes('@')&&ipemail.includes(".com"))){
+    erremail.innerHTML="email address must contain @ and .com"
+    erremail.style.color="red"
+    return false
+}
+
+else if(ipnumber=="" ){
+
+    erremail.innerHTML=""
+  errnumber.innerHTML="please enter your number"
+  errnumber.style.color="red"
+  return false
+}
+else if( isNaN(ipnumber)){
+  erremail.innerHTML=""
+  errnumber.innerHTML="please enter number only"
+  errnumber.style.color="red"
+  return false
+}
+else if(ipnumber.length!==10){
+  erremail.innerHTML=""
+  errnumber.innerHTML="please enter 10 digit number "
+  errnumber.style.color="red"
+  return false
+}
+
+else if(ippassword=="" ){
+    erremail.innerHTML=""
+        errpass.innerHTML="please enter your password"
+        errpass.style.color="red"
+        return false
+        
+}
+
+else if(ipcpass==""){ 
+     errpass.innerHTML=""
+    errcpass.innerHTML="please enter your confrim password"
+    errcpass.style.color="red"
+    return false
+}
+else if(ippassword!=ipcpass){
+document.querySelector("#cpass").value="";
+document.querySelector("#pass").value="";
+document.querySelector("#cpass").focus();  //to bring cusror on place where we want user to enter pass
+    errpass.innerHTML="please enter correct password"
+    errpass.style.color="red"
+    return false
+    }
+
+else if(!( ippassword.match([/1234567890/]&&ippassword.match([/ !@#$%^&*()/])&&ippassword.match([/ a-z/])&&ippassword.match([/ A-Z/])))){
+    errpass.innerHTML="please enter correct password"
+    errpass.style.color="green"
+    return false  
+}
+}
