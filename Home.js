1a01030d161ad47b919fa2c64d0fbfc456a6eaea
ishika <ishikatakhtani04@gmail.com>
2025-02-25 17:10:29 +0530

    update

--- Home.js
+++ Home.js
@@ -46,4 +46,36 @@
         pagination: {
           el: ".swiper-pagination",
         },
+        autoplay :{
+          delay:3000,
+        }
+      });
+
+
+
+
+
+      const progressCircle = document.querySelector(".autoplay-progress svg");
+      const progressContent = document.querySelector(".autoplay-progress span");
+      var swiper = new Swiper(".mySwiper1", {
+        spaceBetween: 30,
+        centeredSlides: true,
+        autoplay: {
+          delay: 2500,
+          disableOnInteraction: false
+        },
+        pagination: {
+          el: ".swiper-pagination",
+          clickable: true
+        },
+        navigation: {
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev"
+        },
+        on: {
+          autoplayTimeLeft(s, time, progress) {
+            progressCircle.style.setProperty("--progress", 1 - progress);
+            progressContent.textContent = `${Math.ceil(time / 1000)}s`;
+          }
+        }
       });
