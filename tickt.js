2abb90c00a4f851f2e1e72d7f569be97cd7aaf51
ishika <ishikatakhtani04@gmail.com>
2025-03-02 23:34:02 +0530

    update

---
+++ tickt.js
@@ -0,0 +1,74 @@
+let fetchdata=async()=>{
+
+    let url='http://localhost:3000/flight'
+
+    let res= await fetch(url,{method:"GET"})
+    let data= await res.json()
+
+    console.log(data);
+    
+
+    let a=document.querySelector("#dat")
+    data.map((e)=>{
+        a.innerHTML+=(
+            `
+            <tr>
+            <td>${e.inpdep}</td>
+             <td>${e.inpdes}</td>
+              <td>${e.inpdate}</td>
+               <td>${e.inprdate}</td>
+             <td>${e.inpsel}</td>
+             <td>${e.inpnum}</td> 
+            <td>${e.inppas}</td>   
+            <td>${e.inadult}</td> 
+            <td>${e.inpchild}</td> 
+            </tr>
+            `
+        )
+    })
+
+}
+
+let del=(id)=>{
+    let url='http://localhost:3000/flight/${id}'
+fetch(url , {method:"DELETE"})
+}
+
+
+let ins=()=>{
+    let sdep=document.querySelector("#inpdep").value
+    let sdes=document.querySelector("#inpdes").value
+    let sdate=document.querySelector("#inpdate").value
+    let sdater=document.querySelector("#inprdate").value
+    let ssel=document.querySelector("#inpsel").value
+    let snum=document.querySelector("#inpnum").value
+    let spass=document.querySelector("#inppass").value
+    let sadult=document.querySelector("#inpadult").value
+    let schild=document.querySelector("#inpchild").value
+
+
+ let url='http://localhost:3000/flight/${id}'
+
+ fetch(url,{
+    method:"POST",
+    headers:{
+        "Content-type":"application/json"
+    },
+    body:JSON.stringify (
+        {
+    "inpdep":sdep,
+    "inpdes":sdes,
+    "inpdate":sdate,
+    "inprdate":srdate,
+    "inpsel":ssel,
+    "inpnum":snum,
+    "inppass":spass,
+    "inpadult":sadult,
+    "inpchild":schild
+    } )
+ })
+location.href="tickcnf.html";
+return false
+}
+
+let fo
